9723a2b58ab1c2dcf28cd5acfbf76788494b4358
Junior Fernandes <jrfernandessi@users.noreply.github.com>
2018-03-14 20:28:30 -0300

    inicializando

---
+++ src/main/java/com/analistajunior/cras/ControleEstoqueApiApplication.java
@@ -0,0 +1,12 @@
+package com.analistajunior.cras;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class ControleEstoqueApiApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(ControleEstoqueApiApplication.class, args);
+	}
+}
