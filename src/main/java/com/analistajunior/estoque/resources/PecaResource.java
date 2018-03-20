9e16f59fca15635e9e954f773c20aa53abf436c1
Junior Fernandes <junior.fernandes@sti.ufc.br>
2018-03-20 17:28:58 -0300

    adicionando a anotation crossorigin

--- src/main/java/com/analistajunior/estoque/resources/PecaResource.java
+++ src/main/java/com/analistajunior/estoque/resources/PecaResource.java
@@ -7,6 +7,7 @@ import javax.validation.Valid;
 
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.CrossOrigin;
 import org.springframework.web.bind.annotation.DeleteMapping;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PathVariable;
@@ -33,6 +34,7 @@ public class PecaResource {
 		return ResponseEntity.ok().body(pecas);
 	}
 	
+	@CrossOrigin(origins = "http://localhost:4200")
 	@PostMapping
 	public ResponseEntity<Peca> salvar(@Valid @RequestBody Peca peca){
 		Peca obj = service.salvar(peca);
